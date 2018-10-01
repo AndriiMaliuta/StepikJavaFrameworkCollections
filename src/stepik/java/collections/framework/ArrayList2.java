8d270dfcf0a70877f2bcf4b1cac78f8435df823f
anma0513 <mbfAATwb2408>
2018-10-01 17:37:56 +0300

    ArrayList2 added

---
+++ src/stepik/java/collections/framework/ArrayList2.java
@@ -0,0 +1,13 @@
+package stepik.java.collections.framework;
+
+import java.util.List;
+
+public class ArrayList2 {
+
+    static List<String> changeList(List<String> list){
+        //paste your code here
+
+        return list;
+    }
+
+}
